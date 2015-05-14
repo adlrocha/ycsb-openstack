a3baf73c5c1fad0405ea0d54f18976c079eda027
Yuning Hui <huiyuningbj@gmail.com>
2015-05-14 18:27:45 +0200

    Added timing management in the auto script vmtest.py on Frodo.
    Tested with two virtual machines running sequentially for every five minutes(See the "config" file in folder "Frodo").

--- General Test Scripts/Frodo/vmtest.py
+++ General Test Scripts/Frodo/vmtest.py
@@ -1,11 +1,14 @@
 import string
 import commands
 import time
+from threading import Timer
 
 fwlm=open('workloadmodel','r')
 swlm=fwlm.read()
 fswm=open('startworkloadmodel','r')
 sswm=fswm.read()
+
+namecount=0
 
 def generatefile(config):
 	tmpwl=swlm
@@ -35,20 +38,29 @@ def getfreeip():
         if(i.find(blank)!=-1):
             return i[2:13]
 
-	
+
+def deletevm(vmid):
+    ret=commands.getoutput("nova delete " + vmid)
+
 	
 def startvm(config):
+    global namecount
+    
     generatefile(config)	
     freeip=getfreeip()
+    namecount=namecount+1
 	
-    ret=commands.getoutput("nova boot --flavor 2 --image 20cf5437-6eea-4498-9ae0-125a4f973a11 --key-name huiyuning --nic net-id=f3327bd6-9658-425e-be09-44c6eb14e603 aaa")
+    ret=commands.getoutput("nova boot --flavor 2 --image c75310f8-cadd-49ee-944f-1aa0d3facdf3 --key-name huiyuning --nic net-id=f3327bd6-9658-425e-be09-44c6eb14e603 " + str(namecount))
     m=ret.splitlines()
     vmid=m[20][41:77]
     time.sleep(1)
     commands.getoutput("nova floating-ip-associate " + vmid + " " + freeip)
     print freeip
+    Timer(int(config[1]),deletevm,(vmid,)).start()
+    
 
-
+def deletevm(vmid):
+    ret=commands.getoutput("nova delete " + vmid)
 
  
 
@@ -56,8 +68,6 @@ fconfig=open('config','r')
 fconfig.readline()
 for line in fconfig:
 	s=line.split()
-	startvm(s)
-
-
-
+	Timer(int(s[0]),startvm,(s,)).start()
+fconfig.close()
 
